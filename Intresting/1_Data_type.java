fe89f8988720a12f41dabf1f9a93a71073cdc7fc
avi4512 <144973797+avi4512@users.noreply.github.com>
2025-04-17 16:01:19 +0530

    Create 1_Data_type.java

---
+++ Intresting/1_Data_type.java
@@ -0,0 +1,16 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		Object Variable1 = 75.3f;
+		Object Variable2 = 12;
+		Object Variable3 = 23.4d;
+		
+		System.out.println(Variable1.getClass());
+		System.out.println(Variable2.getClass().getName());
+		System.out.println(Variable3.getClass().getSimpleName());
+
+	}
+
+}
