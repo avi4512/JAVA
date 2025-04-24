c004ade2db90635f8338392c7df6986278fe4b17
avi4512 <144973797+avi4512@users.noreply.github.com>
2025-04-24 11:29:28 +0530

    Add files via upload

---
+++ Arrays/WithoutSize2.java
@@ -0,0 +1,26 @@
+package com.learn.arrays1;
+
+public class WithoutSize2 {
+
+	public static void main(String[] args) {
+		
+		// without mention the Size of the Array
+		
+		int[] arr = {10,20,30,40,50};
+		
+		//Size of the Array
+		int arrSize = arr.length;
+		System.out.println("Size of the array: "+arrSize);
+		
+		// Printing Array
+		System.out.println("Array Element At index1 : " + arr[0]);
+		System.out.println("Array Element At index2 : " + arr[1]);
+		System.out.println("Array Element At index3 : " + arr[2]);
+		System.out.println("Array Element At index4 : " + arr[3]);
+		System.out.println("Array Element At index5 : " + arr[4]);
+		
+		
+
+	}
+
+}
